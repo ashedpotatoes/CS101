63139e12a5363d06d9f059708a7c88f8f8c75c21
ashton weeks <91101801+ashedpotatoes@users.noreply.github.com>
2021-12-02 19:45:02 -0600

    Update Main.py

--- Week12/Main.py
+++ Week12/Main.py
@@ -1,3 +1,14 @@
+# ashton weeks
+# awy9n@umsystem.edu
+
+# algorithm: this program imports turtle and functions from different classes in order to successfully draw a circle or square and fill them both.
+# boxfilled and circlefilled both inherit from box and circle respectively, and get their starting points and outline colors from point
+# box utilizes a loop in order to move the cursor forward and turn, and circle makes a continous shape based on the radius. 
+# box and circle both have a draw again function that their child classes inherit; and by using that draw again function
+# boxfilled and circlefilled know when to start and stop filling as well as color
+# each child class inherits __init__ attributes from the parent
+# parameters are stated explicitly in main
+
 import turtle
 from Point import *
 from Box import *
@@ -10,3 +21,5 @@ from CircleFilled import *
 #b.draw()
 c = CircleFilled(50, -100, 30, "red", "green")
 c.draw()
+
+# special error handing & other notes: this lab was pretty straight forward so not much is noteworthy or exceptional 
