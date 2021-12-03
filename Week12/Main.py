415ee077e2ad7e7aa6b7e266c8be120359912c11
ashedpotatoes <awy9n@umsystem.edu>
2021-12-02 19:38:56 -0600

    week 12 turtle lab

---
+++ Week12/Main.py
@@ -0,0 +1,12 @@
+import turtle
+from Point import *
+from Box import *
+from BoxFilled import *
+from Circle import *
+from CircleFilled import *
+
+
+#b = BoxFilled(-100, 100, 50, 20, "blue", "red")
+#b.draw()
+c = CircleFilled(50, -100, 30, "red", "green")
+c.draw()
